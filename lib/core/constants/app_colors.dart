288ce7b185ba2349bb427db05040d286731b71b4
Mohamed Attia <attia3104@gmail.com>
2024-06-23 05:20:27 +0300

    app strings added successfully

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color appBarColor = Color(0xffFFBD2E);
+  static const Color shopNowBtnColor = Color(0xffF44236);
+  static const Color greenBtnColor = Color(0xff4CB050);
+  static const Color showDealBtnColor = Color(0xff2196F3);
+}
---
+++ lib/features/home/presentation/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Welcome to the Home Screen!'),
+    );
+  }
+}
