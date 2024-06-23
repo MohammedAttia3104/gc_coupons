3ae1054d81683fcb060eb6d8ae2ca1c9f97f95be
Mohamed Attia <attia3104@gmail.com>
2024-06-23 05:20:40 +0300

    app strings added successfully

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,29 @@
+abstract class AppStrings {
+  static const String appName = 'GC Coupons';
+  static const String home = 'Home';
+  static const String allStores = 'All Stores';
+  static const String allCategories = 'All Categories';
+  static const String favourites = 'Favourites';
+  static const String popularStores = 'Popular Stores';
+  static const String searchStores = 'Search Stores';
+  static const String search = 'Search';
+  static const String trendingCoupons = 'Trending Coupons';
+  static const String exclusive = 'Exclusive';
+  static const String showCoupon = 'Show Coupon';
+  static const String shopNow = 'Shop Now';
+  static const String viewCategories = 'view categories';
+  static const String noFavouritesStoredYet = 'No favourites stored yet..';
+  static const String searchForYourFavouriteStores =
+      'Search for your favourite stores!';
+  static const String copy = 'Copy';
+  static const String description = 'Description';
+  static const String getDeal = 'Get Deal';
+  static const String couponsAndDeals = 'Coupons & Deals';
+
+  // Drawer Tabs
+  static const String homeTab = 'Home';
+  static const String browseCategories = 'Browse Categories';
+  static const String advertiseWithUs = 'Advertise with us';
+  static const String contactUs = 'Contact Us';
+  static const String privacyPolicy = 'Privacy Policy';
+}
