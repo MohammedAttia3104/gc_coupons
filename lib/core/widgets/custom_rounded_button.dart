2d32d8f447e99d02d374685d29cb25293c26e317
Mohamed Attia <attia3104@gmail.com>
2024-06-24 09:56:29 +0300

    build custom rounded button

---
+++ lib/core/widgets/custom_rounded_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gc_coupons/core/constants/app_colors.dart';
+import 'package:gc_coupons/core/constants/app_strings.dart';
+import 'package:gc_coupons/core/constants/app_styles.dart';
+
+class CustomRoundedButton extends StatelessWidget {
+  final Function()? onPressed;
+  final String buttonText;
+  final TextStyle? textStyle;
+  final double buttonHeight;
+  final double buttonWidth;
+  final Color? buttonColor;
+
+  const CustomRoundedButton({
+    super.key,
+    this.onPressed,
+    required this.buttonText,
+    this.textStyle,
+    required this.buttonHeight,
+    required this.buttonWidth,
+    this.buttonColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor ?? AppColors.showDealBtnColor,
+        minimumSize: Size(buttonWidth, buttonHeight),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        buttonText,
+        style: textStyle,
+      ),
+    );
+  }
+}
