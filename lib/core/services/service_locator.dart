f7a4a5e316b44b49ff43cdf53f02f20dcbe38ec4
Mohamed Attia <attia3104@gmail.com>
2024-06-26 03:37:07 +0300

    add trending coupons service locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,8 +1,13 @@
 import 'package:gc_coupons/features/home/data/data_sources/popular_stores/popular_stores_remote_data_source.dart';
 import 'package:gc_coupons/features/home/data/data_sources/popular_stores/popular_stores_local_data_source.dart';
+import 'package:gc_coupons/features/home/data/data_sources/trending_coupons/trending_coupons_local_data_source.dart';
 import 'package:gc_coupons/features/home/data/repositories/popular_stores_repository.dart';
+import 'package:gc_coupons/features/home/data/repositories/trending_coupons_repository.dart';
 import 'package:gc_coupons/features/home/presentation/controllers/popular_stores_cubit/popular_stores_cubit.dart';
+import 'package:gc_coupons/features/home/presentation/controllers/trending_coupons_cubit/trending_coupons_cubit.dart';
 import 'package:get_it/get_it.dart';
+
+import '../../features/home/data/data_sources/trending_coupons/trending_coupons_remote_data_source.dart';
 
 final sl = GetIt.instance;
 
@@ -16,5 +21,20 @@ class ServiceLocator {
         () => PopularStoresRepositoryImpl(sl(), sl()));
 
     sl.registerFactory<PopularStoresCubit>(() => PopularStoresCubit(sl()));
+
+    //Trending Coupons
+    sl.registerLazySingleton<TrendingCouponsRemoteDataSource>(
+        () => TrendingCouponsRemoteDataSourceImpl());
+
+    sl.registerLazySingleton<TrendingCouponsLocalDataSource>(
+      () => TrendingCouponsLocalDataSourceImpl(),
+    );
+    sl.registerLazySingleton<TrendingCouponsRepository>(
+        () => TrendingCouponsRepositoryImpl(
+              sl(),
+              sl(),
+            ));
+
+    sl.registerFactory<TrendingCouponsCubit>(() => TrendingCouponsCubit(sl()));
   }
 }
--- lib/features/home/presentation/widgets/trending_coupons_list_view.dart
+++ lib/features/home/presentation/widgets/trending_coupons_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gc_coupons/core/constants/size_config.dart';
+import 'package:gc_coupons/features/home/presentation/controllers/trending_coupons_cubit/trending_coupons_cubit.dart';
 import 'package:gc_coupons/features/home/presentation/widgets/trending_coupon_item.dart';
 
 class TrendingCouponsListView extends StatelessWidget {
@@ -9,17 +11,21 @@ class TrendingCouponsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      shrinkWrap: true,
-      padding: EdgeInsets.zero,
-      scrollDirection: Axis.vertical,
-      physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (BuildContext context, int index) =>
-          const TrendingCouponItem(),
-      separatorBuilder: (BuildContext context, int index) => SizedBox(
-        height: AppPadding.padding8h,
+    return BlocProvider<TrendingCouponsCubit>(
+      create: (context) => SubjectBloc(),
+      child: ListView.separated(
+        shrinkWrap: true,
+        padding: EdgeInsets.zero,
+        scrollDirection: Axis.vertical,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (BuildContext context, int index) =>
+        const TrendingCouponItem(),
+        separatorBuilder: (BuildContext context, int index) =>
+            SizedBox(
+              height: AppPadding.padding8h,
+            ),
+        itemCount: 10,
       ),
-      itemCount: 10,
     );
   }
 }
